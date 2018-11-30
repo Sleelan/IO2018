4d04ce90ec7ae0afd27688f09c4f3c1a9b8458bf
Sleelan <sleelanzolw@gmail.com>
2018-11-30 09:33:44 +0200

    Added NetBeans project

---
+++ lab9/wypozyczalnia/src/wypozyczalnia/EgzemplarzGry.java
@@ -0,0 +1,20 @@
+package wypozyczalnia;
+
+public class EgzemplarzGry {
+    private int id;
+    private Gra gra;
+    private Rezerwacja rezerwacja;
+    
+    EgzemplarzGry(int newID, Gra newGra){
+        
+    }
+    
+    EgzemplarzGry(int newID, Gra newGra, Rezerwacja newRezerwacja){
+        
+    }
+    
+    @Override
+    public String toString(){
+        return null;
+    }
+}
