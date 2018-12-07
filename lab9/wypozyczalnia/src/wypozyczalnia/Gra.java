884bd9ddef9cb1a23a6dbd5cf21a6c7c02b22908
Sleelan <sleelanzolw@gmail.com>
2018-12-07 10:41:49 +0200

    Added an override of equals() method in Gra class.

--- lab9/wypozyczalnia/src/wypozyczalnia/Gra.java
+++ lab9/wypozyczalnia/src/wypozyczalnia/Gra.java
@@ -2,11 +2,12 @@ package wypozyczalnia;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 public class Gra {
     private String tytul;
     private String wydawnictwo;
-    private int rokWydania;
+    private int rokWydania = -1;
     private String platforma;
     private List<EgzemplarzGry> egzemplarze;
     
@@ -32,5 +33,29 @@ public class Gra {
     @Override
     public String toString(){
         return null;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        Gra gra = (Gra)o;
+        if(gra.tytul != null && this.tytul != gra.tytul)
+            return false;
+        if(gra.platforma != null && this.platforma != gra.platforma)
+            return false;
+        if(gra.rokWydania != -1 && this.rokWydania != gra.rokWydania)
+            return false;
+        if(gra.wydawnictwo != null && this.wydawnictwo != gra.wydawnictwo)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.tytul);
+        hash = 17 * hash + Objects.hashCode(this.wydawnictwo);
+        hash = 17 * hash + this.rokWydania;
+        hash = 17 * hash + Objects.hashCode(this.platforma);
+        return hash;
     }
 }
