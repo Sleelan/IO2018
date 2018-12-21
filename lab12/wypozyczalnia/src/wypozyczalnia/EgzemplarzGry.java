1728c3c1e2234cd01242a1c7f6687b180b1a82a4
Sleelan <sleelanzolw@gmail.com>
2018-12-21 09:29:18 +0200

    Added lab12 folder, updated .gitignore

---
+++ lab12/wypozyczalnia/src/wypozyczalnia/EgzemplarzGry.java
@@ -0,0 +1,53 @@
+package wypozyczalnia;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class EgzemplarzGry {
+    private int id;
+    private Gra gra;
+    private List<Rezerwacja> rezerwacje = new ArrayList<Rezerwacja>();
+    
+    EgzemplarzGry(int newID, Gra newGra){
+        this.id = newID;
+        this.gra = newGra;
+    }
+    
+    public boolean czyTerminKolidujeZRezerwacjami(Date terminPrzedluzony){
+        for(Rezerwacja r : rezerwacje){
+            if(r.czyKolidujeZTerminem(terminPrzedluzony)==true){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Rezerwacja wyszukajRezerwacje(int idKlienta){
+        return null;
+    }
+    
+    public void przypiszRezerwacje(Rezerwacja rezerwacja){
+        rezerwacje.add(rezerwacja);
+    }
+    
+    @Override
+    public String toString(){
+        String result = "Gra: " + gra.getTytul() + ", ID: " + id;
+        result += "\tRezerwacje:";
+        for(Rezerwacja r : rezerwacje){
+            result += "\n\t" + r.toString();
+        }
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        EgzemplarzGry e = (EgzemplarzGry)o;
+        if(this.gra != e.gra)
+            return false;
+        if(this.id != e.id)
+            return false;
+        return true;
+    }
+}
---
+++ lab12/wypozyczalnia/src/wypozyczalnia/Rezerwacja.java
@@ -0,0 +1,50 @@
+package wypozyczalnia;
+import java.util.Date;
+
+public class Rezerwacja {
+    private EgzemplarzGry wypozyczonyEgzemplarz;
+    private Klient wypozyczajacyKlient;
+    private Date dataWygasniecia;
+    private Date terminStartu;
+    private boolean wypozyczona;
+    
+    Rezerwacja(EgzemplarzGry newEgzemplarzGry, Klient newKlient, Date newTerminWygasniecia){
+        wypozyczonyEgzemplarz = newEgzemplarzGry;
+        wypozyczajacyKlient = newKlient;
+        dataWygasniecia = newTerminWygasniecia;
+        terminStartu = new Date();
+        wypozyczona = false;
+    }
+    
+    Rezerwacja(EgzemplarzGry newEgzemplarzGry, Klient newKlient, Date newTerminWygasniecia, boolean newWypozyczona){
+        wypozyczonyEgzemplarz = newEgzemplarzGry;
+        wypozyczajacyKlient = newKlient;
+        dataWygasniecia = newTerminWygasniecia;
+        terminStartu = new Date();
+        wypozyczona = newWypozyczona;
+        
+    }
+    
+    public EgzemplarzGry getEgzemplarz(){
+        return this.wypozyczonyEgzemplarz;
+    }
+    
+    public void przedluz(Date terminPrzedluzony){
+        if (wypozyczonyEgzemplarz.czyTerminKolidujeZRezerwacjami(terminPrzedluzony) == false){
+            dataWygasniecia = terminPrzedluzony;
+        }
+    }
+    
+    public boolean czyKolidujeZTerminem(Date termin){
+        if (terminStartu.after(termin) == true){
+            return true;
+        }
+        return false;
+    }
+            
+    @Override
+    public String toString(){
+        String result = "Zarezerwowane przez klienta o " + wypozyczajacyKlient + ", od " + terminStartu + " do " + dataWygasniecia;
+        return result;
+    }
+}
